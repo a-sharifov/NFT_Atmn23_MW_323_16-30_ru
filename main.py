5cd78948153f99fda9073c498b82457059a372dc
Akber Sharifov <akber.sharifov2004@gmail.com>
2025-06-04 17:46:55 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+#a = 0 # int - 1, 2, 3, 4 .. 99
+#b = 2.2 # float - 1.333, 3.4923, 291.0
+#c = "1ddd11" # String - "hello world" "12ided"
+#d = False # Logic (Boolean) - True or false
+#from traceback import print_tb
+
+#print(b)
+#print(int(b))
+#print(int(c))
+#print(int(d))
+
+
+#print(float(a))
+#print(bool(2.0))
+
+#a = int(input("Enter first value: "))
+#b = int(input("Enter second value: "))
+
+#print(a + b)
+#print(a - b)
+#print(a * b)
+#print(a / b)
+#print(a % b)
+#print(a // b)
+#print(a ** b)
+
+#c = "Hello world"
+#c.count()
+#c.lower()
+#print(c.capitalize())
+#print(c.upper())
+
+
+#a = ["apple", "cherry", "pie", "juice"]
+#a.append("blueberry")
+#a.pop()
+#a.remove()
+#a.insert(1, "berry")
+#b = a.reverse()
+
+
+#a_immutable = ("apple", "cherry", "pie", "juice")
+
+#c = {"apple", "cherry", "pie", "apple"}
+
+#c.intersection()
+#c.difference()
+#c.symmetric_difference()
+#c.union()
+#print(c)
+
+#d = {"q": "Alex", "123": "Bob", }
+
+#print(d["q"])
+#print(d.keys())
+#print(d.values())
